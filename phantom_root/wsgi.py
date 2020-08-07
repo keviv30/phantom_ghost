ccf90935fcf447fbd0b8e3047b8551828ee76ce2
Vivek Santhirababu <keviv365@gmail.com>
2020-08-07 23:24:46 +0100

    Update env variables

--- phantom_root/wsgi.py
+++ phantom_root/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phantom_root.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phantom_root.settings.local')
 
 application = get_wsgi_application()
