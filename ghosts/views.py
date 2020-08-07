16d007417e589e2e668860ad5c3c21e4e11a7009
Vivek Santhirababu <keviv365@gmail.com>
2020-08-07 22:00:16 +0100

    Basic home page view

--- ghosts/views.py
+++ ghosts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    Home page for blogs site
+    """
+    context = {}
+    return render(request, 'ghosts/index.html', context)
